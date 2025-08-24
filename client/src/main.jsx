35106efe0cfe1f687bf96d170264f5996e1dc169
Supreeth kumar <jsupreeth2005@gmail.com>
2025-08-24 17:08:46 +0530

    submitting changes-2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from "react-router-dom"
 import ReactDOM from "react-dom/client";
 import App from './App.jsx'
-import OccasioContextProvider from "./context/occasioContext.jsx";
+import OccasioContextProvider from "./context/OccasioContext.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
