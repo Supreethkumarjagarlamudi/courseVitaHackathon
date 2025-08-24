da20c3ba93371a6c22e064b50ea356fd78b1f05b
Supreeth kumar <jsupreeth2005@gmail.com>
2025-08-24 18:12:35 +0530

    fixing CORS

--- backend/routes/paymentRoute.js
+++ backend/routes/paymentRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { requireAuth } from '../middlewares/middleware.js';
-import { createOrder, verifyPayment, getPaymentStatus, testRazorpayConfig, testEmailConfig } from '../controllers/paymentController.js';
+import { createOrder, verifyPayment, getPaymentStatus } from '../controllers/paymentController.js';
 
 const paymentRouter = express.Router();
 
