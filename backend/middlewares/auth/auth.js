a9655690fc9afb436dd10a05c41f8f36a0b7ee0e
J.Supreeth kumar <133553232+Supreethkumarjagarlamudi@users.noreply.github.com>
2025-08-25 17:11:26 +0530

    Update auth.js

--- backend/middlewares/auth/auth.js
+++ backend/middlewares/auth/auth.js
@@ -35,7 +35,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/api/auth/google/callback",
+      callbackURL: "https://occasio-backend-cyop.onrender.com/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
